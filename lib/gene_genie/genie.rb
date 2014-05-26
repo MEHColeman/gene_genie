6a47bf2cf234d091702376205dd545f22f9b9f0e
Mark Coleman <m@rkcoleman.co.uk>
2014-05-26 19:48:17 +0100

    improve the api - change Gene output to a hash
    I don't want different layers of abstraction in my interface. A hash goes
    in, so a hash comes out.

--- lib/gene_genie/genie.rb
+++ lib/gene_genie/genie.rb
@@ -3,13 +3,13 @@
 module GeneGenie
 
   # Top level, basic interface for GA optimisation.
-  # Genie will attempt to optimise based on best-guess defaults if non are provided
+  # Genie will attempt to optimise based on best-guess defaults if none are
+  # provided
   # @since 0.0.1
   class Genie
     def initialize(template, fitness_evaluator)
       @template = template
       @fitness_evaluator = fitness_evaluator
-
     end
 
     # Optimise the genes until the convergence criteria are met.
--- spec/gene_genie/genie_spec.rb
+++ spec/gene_genie/genie_spec.rb
@@ -7,62 +7,79 @@ require 'gene_genie'
 module GeneGenie
   describe Genie do
     let(:template) do
-      { a: [1..100], b: [1..100] }
+      { a: 1..100, b: 1..100 }
     end
 
     before do
-      @fitness_evaluator = Class.new do
-        def fitness(params)
-          params.each_value.inject(:*)
-        end
+      @fitness_evaluator = Object.new
+      def @fitness_evaluator.fitness(params)
+        params.each_value.inject(:*)
       end
     end
 
     describe '#initialize' do
-
       it 'is initialised with a template and a fitness evaluator' do
-        genie = Genie.new(@template, @fitness_evaluator)
+        genie = Genie.new(template, @fitness_evaluator)
         assert_kind_of Genie, genie
       end
-
     end
 
     describe '#optimise' do
+      before do
+        @genie = Genie.new(template, @fitness_evaluator)
+      end
+
+      it 'returns true when it successfully optimises' do
+        assert true, @genie.optimise
+      end
 
       it 'optimises' do
-        genie = Genie.new(@template, @fitness_evaluator)
-        assert true, genie.optimise
-        optimised = genie.best
-        assert true, optimised.fitness > 90*90
+        assert true, @genie.optimise
+        assert true, @genie.best_fitness > (90 * 90)
+      end
+
+      it "returns false if it doesn't improve current best_fitness" do
+        template = { a: 1..3, b: 1..3 }
+        genie = Genie.new(template, @fitness_evaluator)
+        assert true, genie.optimise(10)
+        # this will surely find THE optimal result
+        assert false, genie.optimise(10)
       end
 
       it 'takes an optional number_of_generations argument' do
-        genie = Genie.new(@template, @fitness_evaluator)
-        assert true, genie.optimise(1)
-        optimised = genie.best
-        assert optimised.fitness > 2*2
+        assert true, @genie.optimise(1)
       end
-
     end
 
     describe '#best' do
-
-      it 'returns a Gene' do
-        genie = Genie.new(@template, @fitness_evaluator)
-        genie.optimise(1)
-        assert_kind_of Gene, genie.best
+      before do
+        @genie = Genie.new(template, @fitness_evaluator)
       end
 
-      it 'returns the best Gene' do
-        genie = Genie.new(@template, @fitness_evaluator)
-        assert true, genie.optimise(1)
-        optimised_1 = genie.best
-        genie.optimise(20)
-        optimised_20 = genie.best
-        assert true, optimised_1.fitness < optimised_20.fitness
+      it 'returns a Hash, conforming to the supplied template' do
+        @genie.optimise(1)
+        optimised = @genie.best
+        assert_kind_of Hash, optimised
+        template.each do |k, v|
+          refute_nil optimised[k]
+          assert true, optimised[k] >= v.min
+          assert true, optimised[k] <= v.max
+        end
       end
 
+      it 'returns optimised data' do
+        @genie.optimise(1)
+        optimised_1 = @genie.best
+        optimised_1_fitness = @fitness_evaluator.fitness(optimised_1)
+
+        @genie.optimise
+        optimised_many = @genie.best
+        optimised_many_fitness = @fitness_evaluator.fitness(optimised_many)
+
+        assert true, optimised_1_fitness < optimised_many_fitness
+        # this test might not always pass. #statistics
+      end
     end
-  end
 
+  end
 end
