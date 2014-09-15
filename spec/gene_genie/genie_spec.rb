d754e1d5355108ffe7431ab97f674dbcc0f352f7
Mark Coleman <m@rkcoleman.co.uk>
2014-09-15 22:55:29 +0100

    refactor genie specs

--- spec/gene_genie/genie_spec.rb
+++ spec/gene_genie/genie_spec.rb
@@ -6,78 +6,59 @@ require 'gene_genie'
 # See template_spec and fitness_evaluator_spec descriptions of the parameters
 module GeneGenie
   describe Genie do
-    let(:template) do
-      { a: 1..100, b: 1..100 }
-    end
 
-    before do
-      @fitness_evaluator = Object.new
-      def @fitness_evaluator.fitness(params)
-        params.each_value.inject(:*) || 1
-      end
-    end
-
+    let(:genie) { Genie.new(sample_template, sample_fitness_evaluator) }
     describe '#initialize' do
       it 'is initialised with a template and a fitness evaluator' do
-        genie = Genie.new(template, @fitness_evaluator)
         assert_kind_of Genie, genie
       end
     end
 
     describe '#optimise' do
-      before do
-        @genie = Genie.new(template, @fitness_evaluator)
-      end
-
       it 'returns true when it successfully optimises' do
-        assert_equal true, @genie.optimise
+        # assuming such a basic optimisation will work every time
+        assert_equal true, genie.optimise
       end
 
       it 'optimises' do
-        assert_equal true, @genie.optimise
-        assert_equal true, @genie.best_fitness > (90 * 90)
+        assert_equal true, genie.optimise, "didn't improve"
+        assert_equal true, genie.best_fitness > (90 * 90),
+                     "didn't find a good result"
       end
 
       it 'also optimizes' do
-        assert_equal true, @genie.optimize
+        assert_equal true, genie.optimize, 'optimize variant not recognised'
       end
 
       it "returns false if it doesn't improve current best_fitness" do
-        template = { a: 1..3, b: 1..3 }
-        genie = Genie.new(template, @fitness_evaluator)
         first_fitness = genie.best_fitness
-        def @fitness_evaluator.fitness(_)
+        def sample_fitness_evaluator.fitness(_)
           20
         end
-        assert_equal true, genie.optimise(10)
+        assert_equal true, genie.optimise(1)
 
         second_fitness = genie.best_fitness
         assert second_fitness > first_fitness
 
-        def @fitness_evaluator.fitness(_)
+        def sample_fitness_evaluator.fitness(_)
           10
         end
-        assert_equal false, genie.optimise(10)
+        assert_equal false, genie.optimise(1)
 
         third_fitness = genie.best_fitness
         assert third_fitness < second_fitness
       end
 
       it 'takes an optional number_of_generations argument' do
-        assert_equal true, @genie.optimise(1)
+        assert_equal true, genie.optimise(10)
       end
     end
 
     describe '#best' do
-      before do
-        @genie = Genie.new(template, @fitness_evaluator)
-      end
-
       it 'returns a Hash, conforming to the supplied template' do
-        @genie.optimise(1)
-        optimised = @genie.best
+        optimised = genie.best
         assert_kind_of Hash, optimised
-        template.each do |k, v|
+        sample_template.each do |k, v|
           refute_nil optimised[k]
           assert_equal true, optimised[k] >= v.min
           assert_equal true, optimised[k] <= v.max
@@ -85,25 +66,24 @@ module GeneGenie
       end
 
       it 'returns optimised data' do
-        @genie.optimise(1)
-        optimised_1 = @genie.best
-        optimised_1_fitness = @fitness_evaluator.fitness(optimised_1)
+        initial_best = genie.best
+        initial_best_fitness = sample_fitness_evaluator.fitness(initial_best)
 
-        @genie.optimise
-        optimised_many = @genie.best
-        optimised_many_fitness = @fitness_evaluator.fitness(optimised_many)
+        genie.optimise
+        optimised_many = genie.best
+        optimised_many_fitness = sample_fitness_evaluator.fitness(optimised_many)
 
-        assert_equal true, optimised_1_fitness < optimised_many_fitness
+        assert_equal true, initial_best_fitness < optimised_many_fitness,
+                           'fitness did not improve'
         # this test might not always pass. #statistics
       end
     end
 
     describe '#best_fitness' do
       it 'returns the fitness of the best gene' do
-        genie = Genie.new(template, @fitness_evaluator)
         genie.optimise(1)
         optimised = genie.best
-        optimised_fitness = @fitness_evaluator.fitness(optimised)
+        optimised_fitness = sample_fitness_evaluator.fitness(optimised)
         assert_equal true, optimised_fitness == genie.best_fitness
       end
     end
