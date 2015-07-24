8a668caf1568a545a590ce432de645388b42fa6a
Mark Coleman <mark.coleman@mediasp.com>
2015-07-24 13:43:53 +0100

    make simplecov optional
    Swallow errors if the simplecov gem is not present. If you want to use
    it, stick it in a file called Gemfile.local

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -1,5 +1,9 @@
 require 'minitest'
 require 'minitest/autorun'
-require 'simplecov'
 
-SimpleCov.start
+begin
+  require 'simplecov'
+  SimpleCov.start
+rescue LoadError
+  # simplecov not installed. That's OK
+end
