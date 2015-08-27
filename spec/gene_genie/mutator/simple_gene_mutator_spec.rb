40c19e5746224f1aab9d0c32c923c243efacaed6
Mark Coleman <m@rkcoleman.co.uk>
2015-08-27 20:10:01 +0100

    add a simple gene mutator
    This is the most basic gene mutator I can think of. It uses the gene
    template and a mutation probability rate to change the hash values to
    any other valid value.
    
    A gene mutator only needs to respond to .call, so a Proc will do

---
+++ spec/gene_genie/mutator/simple_gene_mutator_spec.rb
@@ -0,0 +1,50 @@
+require 'minitest_helper'
+require 'gene_genie/mutator/simple_gene_mutator'
+
+module GeneGenie
+  describe SimpleGeneMutator do
+    let(:template) { {
+      a: 1..1000,
+      b: 1..1000,
+      c: 1..1000,
+      d: 1..1000,
+      e: 1..1000,
+      f: 1..1000,
+      g: 1..1000,
+    } }
+
+    let(:mutation_rate) { 0.5 }
+
+    let(:valid_hash) { {
+      a: 100,
+      b: 200,
+      c: 300,
+      d: 400,
+      e: 500,
+      f: 600,
+      g: 700,
+    } }
+
+    subject { SimpleGeneMutator.new(template, mutation_rate) }
+
+    describe '#initialize' do
+      it 'is initialised with a gene template and a mutation rate' do
+        gm = SimpleGeneMutator.new(template, mutation_rate)
+        assert_kind_of SimpleGeneMutator, gm
+      end
+    end
+
+    describe '#call' do
+      it 'mutates the hash' do
+        original_hash = Marshal.load(Marshal.dump(valid_hash))
+        assert_equal original_hash, valid_hash
+        refute_equal original_hash, subject.call(valid_hash)
+      end
+
+      it 'returns a valid hash' do
+        # pending
+      end
+    end
+
+  end
+end
