478cbac1fc15186490773fc693d3becc02621aae
Mark Coleman <m@rkcoleman.co.uk>
2022-06-26 17:11:14 +0100

    Make simple sum example a higher dimension problem

--- examples/sum.rb
+++ examples/sum.rb
@@ -2,17 +2,17 @@ require 'gene_genie'
 
 class Summer
   def fitness(params)
-    params.each_value.inject(&:+)
+    params.inject(0) {|acc, values| acc + values.each_value.inject(&:+)}
   end
 end
 
 # the template contains a list of the parameter to optimise and their range of values
-template = {
-  a: [1..10],
-  b: [5..100],
-  c: [0..1],
-  d: [-50..5]
-}
+template = [{
+  a: 1..100,
+  b: 5..1000,
+  c: 0..1,
+  d: -500..50
+}]
 
 # the fitness evaluator is any object that can respond to the fitness method with a float
 # in this case, it sums the values of the parameters.
@@ -23,3 +23,6 @@ genie = GeneGenie::Genie.new(template, fitness_evaluator)
 genie.optimise
 # the best possible result is { a: 10, b: 100, c: 1, d: 5} = 116
 puts genie.best.inspect
+
+# to see this code in action:
+# bundle exec ruby sum.rb
